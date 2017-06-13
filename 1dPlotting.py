f2d43d1f7c3d8b4a0d800d6ad88a45dfeb45a6b3
J.T. Earl <jtearl188@gmail.com>
2017-06-12 22:50:24 -0400

    Cleaned up imports

--- 1dPlotting.py
+++ 1dPlotting.py
@@ -5,9 +5,8 @@ Created on Wed Jun 07 23:21:06 2017
 @author: JT
 """
 import numpy as np
-import matplotlib as mpl
 import matplotlib.pyplot as plt
-#%matplotlib inline
+
 
 np.random.seed(1000)
 y = np.random.standard_normal(20)
@@ -21,6 +20,7 @@ y = np.random.standard_normal(20)
 #plt.axis('tight') # adjusts the axis ranges
 #plt.xlim(-1, 20)
 #plt.xlim(np.min(y.cumsum()) - 1, np.max(y.cumsum()) + 1)
+
 plt.figure(figsize=(7,4)) # fig size is size in (width, height)
 plt.plot(y.cumsum(), 'b', lw=1.5)
 plt.plot(y.cumsum(), 'ro')
